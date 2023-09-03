38b43bcf6bf4d37915cb5a5c948e5803b2b5fc72
Chen Yu <2320052362@qq.com>
2023-09-03 10:13:38 +0800

    Separate phonebook to individual project

--- part3/phonebook_frontend/src/components/Filter.jsx
+++
@@ -1,7 +0,0 @@
-const Filter = ({ searched, handleChangeSearched }) => {
-    return <div>
-        filter shown with<input value={searched} onChange={handleChangeSearched} />
-    </div>
-}
-
-export default Filter
